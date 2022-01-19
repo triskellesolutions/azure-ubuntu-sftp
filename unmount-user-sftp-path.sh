dd229565ec4a992e50c8f9a9326522b79e6f85f5
John Babb <john.babb@triskelle.solutions>
2022-01-19 04:39:59 -0500

    update unmount script

--- unmount-user-sftp-path.sh
+++ unmount-user-sftp-path.sh
@@ -6,14 +6,24 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 user=$1
 
+# sudo killall -u $user
+# sudo umount /home/$user/uploads
+# sudo umount /home/$user/downloads
+# sudo chown -R $user /home/$user
+
 code=0 && response=$(sudo killall -u $user 2>&1) || code=$?
+if [ $code != 0 ]; then
+    echo "killall failed:  ${response}"
+fi
+
+code=0 && response=$(sudo umount /home/$user/uploads  2>&1) || code=$?
 if [ $code != 0 ]; then
     echo "umount failed:  ${response}"
 fi
 
-code=0 && response=$(sudo umount /home/$user/uploads 2>&1) || code=$?
+code=0 && response=$(sudo rm -fr /home/$user/uploads 2>&1) || code=$?
 if [ $code != 0 ]; then
-    echo "umount failed:  ${response}"
+    echo "rm failed:  ${response}"
 fi
 
 code=0 && response=$(sudo umount /home/$user/downloads 2>&1) || code=$?
@@ -21,7 +31,13 @@ if [ $code != 0 ]; then
     echo "umount failed:  ${response}"
 fi
 
-code=0 && response=$(sudo chown -R $user /home/$user 2>&1) || code=$?
+code=0 && response=$(sudo rm -fr /home/$user/downloads 2>&1) || code=$?
+if [ $code != 0 ]; then
+    echo "rm failed:  ${response}"
+fi
+
+code=0 && response=$(sudo chown -R $user:"sftpusers" /home/$user 2>&1) || code=$?
 if [ $code != 0 ]; then
     echo "chown failed:  ${response}"
 fi
+
